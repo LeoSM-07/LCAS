446ca1d5e6019f38311b1cb63955c2240e40c0f4
LeoSM_07 <69401930+LeoSM-07@users.noreply.github.com>
2023-10-22 08:56:07 -0700

    Initial Commit

---
+++ LCAS/main.cpp
@@ -0,0 +1,14 @@
+//
+// main.cpp
+// LCAS
+//
+// Created by LeoSM_07 on 10/22/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
